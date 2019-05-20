62dda190849c63d8a166de68b9c25e4f16149c05
sgadh99 <50816245+sgadh99@users.noreply.github.com>
2019-05-19 20:44:49 -0700

    Create MathProblems.java

---
+++ MathProblems.java
@@ -0,0 +1,31 @@
+/*Sathya Gadhiraju
+ Solves math problems.*/
+
+public class MathProblems{ //class name is MathProblems
+	public static void main(String [] args){//main method
+		double x; //declaration 
+		x=0.0; //initialization
+		x= 5.6*17/3-48%11/2; //processing
+		System.out.println("5.6*17/3-48%11/2="+x); //output
+		
+		int y; //declaration
+		y=9; //initialization
+	    y= 16-17*(5%6/3); //processing
+		System.out.println("16-17*(5%6/3="+y); //output
+		
+		char z; //declaration
+		z='a'; //initialization
+		z= (char)(56/2+17*3-9%9); //processing
+		System.out.println("56/2+17*3-9%9="+z); //output
+		
+		int t; //declaration
+		t=4; //initialization
+		t= (int)((double)9/12*3-6.5); //processing
+		System.out.println("9/12*3-6.5="+t); //output
+		
+		double g; //declaration
+		g=6.0; //initialization
+		g= (int)('g')+(double)('l'); //processing
+		System.out.println("'g'+('l')="+g); //output
+	}
+}
